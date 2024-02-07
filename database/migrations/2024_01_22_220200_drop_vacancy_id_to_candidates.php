72cdc4fbb82c8faba899fac7d05b8a516b283c9a
Yayang Aditya Wijaya <yayang.aditya@lamsolusi.com>
2024-02-07 14:22:13 +0700

    testing

--- database/migrations/2024_01_22_220200_drop_vacancy_id_to_candidates.php
+++ database/migrations/2024_01_22_220200_drop_vacancy_id_to_candidates.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->dropColumn([
                 'step',
                 'terminated',
-                'foreign_id'
+                'vacancy_id'
             ]);
         });
     }
